d4ef7d40d0feb6a7798840c89f5a64c364a5585d
cowglow <cowglow@gmail.com>
2021-11-30 19:30:22 +0000

    Deploying to gh-pages from @ compose-us/wichteln@f7ac0cca0be65093c5ef32b489f56b0058d87589 🚀

---
+++ _app/pages/index.svelte-0ab367ed.js
@@ -0,0 +1,31 @@
+import{S as nt,i as rt,s as at,M as st,j as V,m as K,o as U,x as X,u as Z,v as tt,N as lt,O as ot,P as it,e as l,t as f,k as D,c as o,a as R,g as h,d as p,n as F,b as g,Q as ct,f as y,F as a}from"../chunks/vendor-52cac395.js";import{p as et}from"../chunks/participants-store-535378ac.js";function ut(b){let e;return{c(){e=f("Let's Begin")},l(i){e=h(i,"Let's Begin")},m(i,r){y(i,e,r)},d(i){i&&p(e)}}}function pt(b){let e,i,r,u,d,_,n,$,O,S,W,x,C,E,I,j,J,H,L,M,G,N,P,q,A,T,Y,z,B,v,m,w;return m=new it({props:{block:!0,size:"lg",color:"success",href:"/participants-form",$$slots:{default:[ut]},$$scope:{ctx:b}}}),{c(){e=l("h1"),i=f("WICHTELN (Secret Santa)"),r=D(),u=l("hr"),d=D(),_=l("div"),n=l("p"),$=l("img"),S=f(`
+			Get started by entering the names of your participants.
+			`),W=l("br"),x=l("br"),C=f(`
+			Make sure you spell their names correctly. If you need to make a correction. Just delete the entry
+			and add it again.
+			`),E=l("br"),I=l("br"),j=f(`
+			When you are ready, click on the "shuffle" button to render a list of links.
+			`),J=l("br"),H=l("br"),L=f(`
+			Click on each name, one by one, to copy the link that you should send the corresponding participant.
+			`),M=l("br"),G=l("br"),N=f(`
+			As you copy each link the list item will turn green. So you will not loose your place.
+			`),P=l("br"),q=l("br"),A=f(`
+			Just becareful to not refresh the page. You will lose your work and will have to start all over
+			again.
+			`),T=l("br"),Y=l("br"),z=f(`
+			We hope you have fun with your Wichteln party.`),B=D(),v=l("p"),V(m.$$.fragment),this.h()},l(t){e=o(t,"H1",{class:!0});var c=R(e);i=h(c,"WICHTELN (Secret Santa)"),c.forEach(p),r=F(t),u=o(t,"HR",{class:!0}),d=F(t),_=o(t,"DIV",{class:!0});var k=R(_);n=o(k,"P",{});var s=R(n);$=o(s,"IMG",{src:!0,alt:!0,class:!0}),S=h(s,`
+			Get started by entering the names of your participants.
+			`),W=o(s,"BR",{}),x=o(s,"BR",{}),C=h(s,`
+			Make sure you spell their names correctly. If you need to make a correction. Just delete the entry
+			and add it again.
+			`),E=o(s,"BR",{}),I=o(s,"BR",{}),j=h(s,`
+			When you are ready, click on the "shuffle" button to render a list of links.
+			`),J=o(s,"BR",{}),H=o(s,"BR",{}),L=h(s,`
+			Click on each name, one by one, to copy the link that you should send the corresponding participant.
+			`),M=o(s,"BR",{}),G=o(s,"BR",{}),N=h(s,`
+			As you copy each link the list item will turn green. So you will not loose your place.
+			`),P=o(s,"BR",{}),q=o(s,"BR",{}),A=h(s,`
+			Just becareful to not refresh the page. You will lose your work and will have to start all over
+			again.
+			`),T=o(s,"BR",{}),Y=o(s,"BR",{}),z=h(s,`
+			We hope you have fun with your Wichteln party.`),s.forEach(p),k.forEach(p),B=F(t),v=o(t,"P",{class:!0});var Q=R(v);K(m.$$.fragment,Q),Q.forEach(p),this.h()},h(){g(e,"class","display-3"),g(u,"class","my-2"),ct($.src,O="/SantaClaus.svg")||g($,"src",O),g($,"alt","Santa Claus"),g($,"class","svelte-1bayy3d"),g(_,"class","content svelte-1bayy3d"),g(v,"class","lead")},m(t,c){y(t,e,c),a(e,i),y(t,r,c),y(t,u,c),y(t,d,c),y(t,_,c),a(_,n),a(n,$),a(n,S),a(n,W),a(n,x),a(n,C),a(n,E),a(n,I),a(n,j),a(n,J),a(n,H),a(n,L),a(n,M),a(n,G),a(n,N),a(n,P),a(n,q),a(n,A),a(n,T),a(n,Y),a(n,z),y(t,B,c),y(t,v,c),U(m,v,null),w=!0},p(t,c){const k={};c&2&&(k.$$scope={dirty:c,ctx:t}),m.$set(k)},i(t){w||(X(m.$$.fragment,t),w=!0)},o(t){Z(m.$$.fragment,t),w=!1},d(t){t&&p(e),t&&p(r),t&&p(u),t&&p(d),t&&p(_),t&&p(B),t&&p(v),tt(m)}}}function ft(b){let e,i;return e=new st({props:{$$slots:{default:[pt]},$$scope:{ctx:b}}}),{c(){V(e.$$.fragment)},l(r){K(e.$$.fragment,r)},m(r,u){U(e,r,u),i=!0},p(r,[u]){const d={};u&2&&(d.$$scope={dirty:u,ctx:r}),e.$set(d)},i(r){i||(X(e.$$.fragment,r),i=!0)},o(r){Z(e.$$.fragment,r),i=!1},d(r){tt(e,r)}}}function ht(b,e,i){let r;return lt(b,et,u=>i(0,r=u)),ot(et,r=[],r),[]}class bt extends nt{constructor(e){super();rt(this,e,ht,ft,at,{})}}export{bt as default};
