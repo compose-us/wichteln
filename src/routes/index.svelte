0fc7c96f6b62d0aa4e536f94120b557f83bcf6e4
Philip Saa <cowglow@gmail.com>
2021-11-30 20:25:23 +0100

    Update intro

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,10 +11,22 @@
 	<hr class="my-2" />
 	<div class="content">
 		<p>
-			<img src="/SantaClaus.svg" alt="Santa Claus" />Get started by entering your participants names
+			<img src="/SantaClaus.svg" alt="Santa Claus" />
+			Get started by entering the names of your participants.
 			<br />
-			This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured
-			content or information.
+			Make sure you spell their names correctly. If you need to make a correction. Just delete the entry
+			and add it again.
+			<br />
+			When you are ready, click on the "shuffle" button to render a list of links.
+			<br />
+			Click on each name, one by one, to copy the link that you should send the corresponding participant.
+			<br />
+			As you copy each link the list item will turn green. So you will not loose your place.
+			<br />
+			Just becareful to not refresh the page. You will lose your work and will have to start all over
+			again.
+			<br />
+			We hope you have fun with your Wichteln party.
 		</p>
 	</div>
 	<p class="lead">
