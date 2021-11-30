5591b436eb4258638f69da328449f19ad2b31007
Philip Saa <cowglow@gmail.com>
2021-11-30 20:28:16 +0100

    Fixed text spacing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,19 +13,19 @@
 		<p>
 			<img src="/SantaClaus.svg" alt="Santa Claus" />
 			Get started by entering the names of your participants.
-			<br />
+			<br /><br />
 			Make sure you spell their names correctly. If you need to make a correction. Just delete the entry
 			and add it again.
-			<br />
+			<br /><br />
 			When you are ready, click on the "shuffle" button to render a list of links.
-			<br />
+			<br /><br />
 			Click on each name, one by one, to copy the link that you should send the corresponding participant.
-			<br />
+			<br /><br />
 			As you copy each link the list item will turn green. So you will not loose your place.
-			<br />
+			<br /><br />
 			Just becareful to not refresh the page. You will lose your work and will have to start all over
 			again.
-			<br />
+			<br /><br />
 			We hope you have fun with your Wichteln party.
 		</p>
 	</div>
