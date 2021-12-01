2302776beab3826476037287465ab7f683ccd5e8
Wiesusenurmitj <49201659+Wiesusenurmitj@users.noreply.github.com>
2021-12-01 12:58:30 +0000

    Deploying to gh-pages from @ compose-us/wichteln@e1a588c7286cb241521da4b3e9876bafef9a060f 🚀

---
+++ _app/pages/index.svelte-6a579c05.js
@@ -0,0 +1,37 @@
+import{S as ie,i as ae,s as le,M as ue,j as Y,m as Z,o as ee,x as te,u as ne,v as se,N as ce,O as oe,P as me,e as a,t as d,k as Q,c as l,a as L,g as f,d as m,n as V,b as k,Q as de,f as p,F as n}from"../chunks/vendor-52cac395.js";import{p as re}from"../chunks/participants-store-535378ac.js";function fe(h){let s;return{c(){s=d("Los geht's")},l(u){s=f(u,"Los geht's")},m(u,i){p(u,s,i)},d(u){u&&m(s)}}}function be(h){let s,u,i,o,g,_,t,$,U,S,W,N,w,E,x,z,A,T,j,I,K,y,C,P,H,q,M,F,G,J,O,B,v,b,R;return b=new me({props:{block:!0,size:"lg",color:"success",href:"/participants-form",$$slots:{default:[fe]},$$scope:{ctx:h}}}),{c(){s=a("h1"),u=d("WICHTELN (Secret Santa)"),i=Q(),o=a("hr"),g=Q(),_=a("div"),t=a("p"),$=a("img"),S=d(`
+			Kleine Anleitung:
+			`),W=a("br"),N=a("br"),w=d(`
+			Trage als erstes die Teilnehmernamen ein.
+			`),E=a("br"),x=a("br"),z=d(`
+			Kontrolliere, ob Du die Namen richtig geschrieben hast. Wenn Du einen Namen korrigieren willst,
+			dann l\xF6sche Deinen Eintrag und schreibe den Namen neu.
+			`),A=a("br"),T=a("br"),j=d(`
+			Wenn Du fertig bist, dann klicke auf den "Shuffle"- Button, um eine Link-Liste erstellen zu lassen.
+			`),I=a("br"),K=a("br"),y=d(`
+			Klicke auf jeden Namen, einer nach dem anderen, um den Link an den passenden Teilnehmer zu verschicken.
+			`),C=a("br"),P=a("br"),H=d(`
+			Wenn Du einen Link kopiert hast, wird dieser gr\xFCn in der Liste angzeigt, so dass Du Deinen Platz
+			nicht verlierst.
+			`),q=a("br"),M=a("br"),F=d(`
+			Aufgepasst! Aktualisiere nicht die Seite unterm Arbeiten, denn sonst sind Deine Eingaben weg und
+			Du musst von vorne beginnen. Das liegt daran, dass wir keine Daten speichern.
+			`),G=a("br"),J=a("br"),O=d(`
+			Wir hoffen, dass Ihr viel Spa\xDF beim Wichteln habt!`),B=Q(),v=a("p"),Y(b.$$.fragment),this.h()},l(e){s=l(e,"H1",{class:!0});var c=L(s);u=f(c,"WICHTELN (Secret Santa)"),c.forEach(m),i=V(e),o=l(e,"HR",{class:!0}),g=V(e),_=l(e,"DIV",{class:!0});var D=L(_);t=l(D,"P",{});var r=L(t);$=l(r,"IMG",{src:!0,alt:!0,class:!0}),S=f(r,`
+			Kleine Anleitung:
+			`),W=l(r,"BR",{}),N=l(r,"BR",{}),w=f(r,`
+			Trage als erstes die Teilnehmernamen ein.
+			`),E=l(r,"BR",{}),x=l(r,"BR",{}),z=f(r,`
+			Kontrolliere, ob Du die Namen richtig geschrieben hast. Wenn Du einen Namen korrigieren willst,
+			dann l\xF6sche Deinen Eintrag und schreibe den Namen neu.
+			`),A=l(r,"BR",{}),T=l(r,"BR",{}),j=f(r,`
+			Wenn Du fertig bist, dann klicke auf den "Shuffle"- Button, um eine Link-Liste erstellen zu lassen.
+			`),I=l(r,"BR",{}),K=l(r,"BR",{}),y=f(r,`
+			Klicke auf jeden Namen, einer nach dem anderen, um den Link an den passenden Teilnehmer zu verschicken.
+			`),C=l(r,"BR",{}),P=l(r,"BR",{}),H=f(r,`
+			Wenn Du einen Link kopiert hast, wird dieser gr\xFCn in der Liste angzeigt, so dass Du Deinen Platz
+			nicht verlierst.
+			`),q=l(r,"BR",{}),M=l(r,"BR",{}),F=f(r,`
+			Aufgepasst! Aktualisiere nicht die Seite unterm Arbeiten, denn sonst sind Deine Eingaben weg und
+			Du musst von vorne beginnen. Das liegt daran, dass wir keine Daten speichern.
+			`),G=l(r,"BR",{}),J=l(r,"BR",{}),O=f(r,`
+			Wir hoffen, dass Ihr viel Spa\xDF beim Wichteln habt!`),r.forEach(m),D.forEach(m),B=V(e),v=l(e,"P",{class:!0});var X=L(v);Z(b.$$.fragment,X),X.forEach(m),this.h()},h(){k(s,"class","display-3"),k(o,"class","my-2"),de($.src,U="/SantaClaus.svg")||k($,"src",U),k($,"alt","Santa Claus"),k($,"class","svelte-xciccd"),k(_,"class","content svelte-xciccd"),k(v,"class","lead")},m(e,c){p(e,s,c),n(s,u),p(e,i,c),p(e,o,c),p(e,g,c),p(e,_,c),n(_,t),n(t,$),n(t,S),n(t,W),n(t,N),n(t,w),n(t,E),n(t,x),n(t,z),n(t,A),n(t,T),n(t,j),n(t,I),n(t,K),n(t,y),n(t,C),n(t,P),n(t,H),n(t,q),n(t,M),n(t,F),n(t,G),n(t,J),n(t,O),p(e,B,c),p(e,v,c),ee(b,v,null),R=!0},p(e,c){const D={};c&2&&(D.$$scope={dirty:c,ctx:e}),b.$set(D)},i(e){R||(te(b.$$.fragment,e),R=!0)},o(e){ne(b.$$.fragment,e),R=!1},d(e){e&&m(s),e&&m(i),e&&m(o),e&&m(g),e&&m(_),e&&m(B),e&&m(v),se(b)}}}function pe(h){let s,u;return s=new ue({props:{$$slots:{default:[be]},$$scope:{ctx:h}}}),{c(){Y(s.$$.fragment)},l(i){Z(s.$$.fragment,i)},m(i,o){ee(s,i,o),u=!0},p(i,[o]){const g={};o&2&&(g.$$scope={dirty:o,ctx:i}),s.$set(g)},i(i){u||(te(s.$$.fragment,i),u=!0)},o(i){ne(s.$$.fragment,i),u=!1},d(i){se(s,i)}}}function he(h,s,u){let i;return ce(h,re,o=>u(0,i=o)),oe(re,i=[],i),[]}class $e extends ie{constructor(s){super();ae(this,s,he,pe,le,{})}}export{$e as default};
