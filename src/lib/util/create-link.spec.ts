d4fa21c9cbdb51ec08891e4a6ee783f1b341592b
Tamara Bogantseva <tamara.bogantseva@compose.us>
2022-01-12 11:44:03 +0100

    pass encoding func as a param in create link func

--- src/lib/util/create-link.spec.ts
+++ src/lib/util/create-link.spec.ts
@@ -1,19 +1,18 @@
 import { createLink } from './create-link'
 import { demoParticipants } from '../demo-participants'
 
-//testing environment? 
-//should we check type of returned value if we are using TS? 
 describe('create link that show assignee', () => {
+    const encode = (params: string) : string => Buffer.from((params)).toString("base64")
     test('check that function does not return undefined', () => {
         expect(createLink('', {
             participant: demoParticipants[0],
             assignment: demoParticipants[1]
-        }, 'buffer')).toBeDefined()
+        }, encode)).toBeDefined()
     })
     test('check that it put exprcted params in the link', () => { 
         expect(createLink('', {
             participant: demoParticipants[0],
             assignment: demoParticipants[1]
-        }, 'buffer' )).toMatch(/(\bLuke\+Skywalker\b)|(\bsecret=TGlhIFNreXdhbGtlcg\b(?:.)*)/g)
+        }, encode )).toMatch(/(\bLuke\+Skywalker\b)&(\bsecret=TGlhIFNreXdhbGtlcg\b(?:.)*)/g)
     })
 })
--- src/lib/util/create-link.ts
+++ src/lib/util/create-link.ts
@@ -2,9 +2,9 @@ import type { Participant } from '$lib/stores/participants-store';
 
 type linkParams = { participant: Participant; assignment: Participant };
 
-export function createLink(url: string, { participant, assignment }: linkParams, encodeMethod: string = 'btoa'): string {
+export function createLink(url: string, { participant, assignment }: linkParams, encode:Function = btoa): string {
 	const assignee = `${participant.firstName} ${participant.lastName}`;
-	const secret = (encodeMethod == 'btoa') ? btoa(`${assignment.firstName} ${assignment.lastName}`) : Buffer.from(`${assignment.firstName} ${assignment.lastName}`).toString("base64");
+	const secret = encode(`${assignment.firstName} ${assignment.lastName}`);
 	const params = new URLSearchParams({ assignee, secret }).toString();
 	return `${url}/secret/?${params}`;
 }
