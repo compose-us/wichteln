06d6b4845da81d2884a25b3616dc0d21e4238c8f
Tamara Bogantseva <tamara.bogantseva@compose.us>
2022-01-12 15:19:23 +0100

    validate shuffle test

---
+++ src/lib/util/validate-shuffle.spec.ts
@@ -0,0 +1,21 @@
+import { validateShuffle } from "./validate-shuffle";
+import { demoParticipants } from '../demo-participants'
+
+describe('check that func return what we expected', () => {
+    it('return true if lists does not match', () => {
+        const shuffledList = [...demoParticipants].sort((a,b) => {
+            const nameA = a.firstName;
+            const nameB = b.firstName;
+            if (nameA < nameB) {
+                return -1;
+            } if (nameA > nameB) {
+                return 1;
+            }
+            return 0
+        });
+        expect(validateShuffle(demoParticipants, shuffledList)).toBeTruthy();
+    });
+    it('return false if there are matches in lists', () => {
+        expect(validateShuffle(demoParticipants, demoParticipants)).toBeFalsy();
+    })
+})
