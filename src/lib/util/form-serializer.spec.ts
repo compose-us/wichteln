23e4a0726e2682638d27b2067f38dd42fe8470b7
Tamara Bogantseva <tamara.bogantseva@compose.us>
2022-01-12 15:19:50 +0100

    form serializer test

---
+++ src/lib/util/form-serializer.spec.ts
@@ -0,0 +1,26 @@
+import { formSerializer } from "./form-serializer";
+
+describe('check if func return correct values', () => {
+    const inputValue = {
+        elements: [
+            {
+                nodeName: 'INPUT',
+                type: 'text',
+                value: 'test name',
+                name: 'firstName'
+            },
+            {
+                nodeName: 'INPUT',
+                type: 'text',
+                value: 'test surname',
+                name: 'lastName'
+            }
+        ]
+    }
+    it('returns an object with expected key value pairs', () => {
+        expect(formSerializer(inputValue)).toEqual(expect.objectContaining({
+            firstName: expect.stringContaining(inputValue.elements[0].value),
+            lastName: expect.stringContaining(inputValue.elements[1].value)
+        }))
+    })
+})
